84817658de0406ab7e0d5cf7811712f2c509cfc0
Martin <mrrolandmartin@gmail.com>
2022-10-13 17:24:03 +0200

    desktop-start

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    // Unset client-side javascript that only works server-side
+    config.resolve.fallback = { fs: false, module: false, path: false };
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    });
+    return config;
+  },
+};
